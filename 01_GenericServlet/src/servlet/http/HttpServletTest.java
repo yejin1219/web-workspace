11b156ecd187b6713b383f46daf1eb126f6548b8
johnmayer <133620338+yejin1219@users.noreply.github.com>
2023-08-01 16:50:58 +0900

    01_GenericServlet

---
+++ 01_GenericServlet/src/servlet/http/HttpServletTest.java
@@ -0,0 +1,29 @@
+package servlet.http;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HttpServletTest
+ */
+public class HttpServletTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body bgColor='yellow'>");
+		out.println("<h2>Hello httpServlet</h2>");
+		out.println("<body><html>");
+		
+		out.close();
+	}
+
+}
