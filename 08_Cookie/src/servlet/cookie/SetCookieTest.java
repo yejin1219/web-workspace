c1e31d9a66d63f73eec00f705cea61e019252ddd
johnmayer <133620338+yejin1219@users.noreply.github.com>
2023-08-07 17:48:54 +0900

    08_Cookie

---
+++ 08_Cookie/src/servlet/cookie/SetCookieTest.java
@@ -0,0 +1,49 @@
+package servlet.cookie;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SetCookieTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * 요청이 들어왔을 때 
+		 * 1. 쿠키 생성 : Cookie c = new Cookie("name","value");
+		 * 2. 생성한 쿠키를 웹 브라우저로 보냄 : response.addCookie(c);
+		 * */
+		Date now = new Date();
+		String id = "kh";
+		
+		//1. 쿠키 생성
+		Cookie c1 = new Cookie("now",Long.toString(now.getTime()));
+		Cookie c2 = new Cookie("id",id);
+		
+		//2. 쿠키의 유효시간을 지정
+		c1.setMaxAge(0); // 0을 넣으면 쿠키 사용안하겠다는 뜻
+		c1.setMaxAge(20); //20초 동안만 쿠키를 사용하겠다.
+		c2.setMaxAge(60*60*24); // 하루
+		
+		//3. 쿠키를 전송
+		response.addCookie(c1);
+		response.addCookie(c2);
+		
+		//4. 페이지 이동
+		response.sendRedirect("GetCookieTest");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
