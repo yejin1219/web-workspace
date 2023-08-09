169a751f036ae624050d4524c23aeae095c89ac0
johnmayer <133620338+yejin1219@users.noreply.github.com>
2023-08-09 17:42:21 +0900

    15_FactoryPattern

---
+++ 15_FactoryPattern/src/pattern/action/Action.java
@@ -0,0 +1,10 @@
+package pattern.action;
+/*
+ * Framework 기술
+ * Strust1 --> Strust2 --> Spring FrameWork
+ * 
+ * */
+public interface Action {
+
+	void execute(); // public abstract
+}
---
+++ 15_FactoryPattern/src/pattern/action/AddAction.java
@@ -0,0 +1,23 @@
+package pattern.action;
+/*
+ * AddAction 컴포넌트
+ * -- > 인터페이스 기반으로 작성된 재사용성이 강한 자바 클래스 
+ * */
+
+
+public class AddAction implements Action {
+
+	@Override
+	public void execute() {
+		/*
+		 * 1. 폼값 받아서
+		 * 2. 객체 생성하고
+		 * 3. DAO리턴받고
+		 * 4. 리턴된 값 바인딩
+		 * 5. Path를 최종적으로 리턴(네비게이션)
+		 * */
+		System.out.println("INSERT OK!");
+	}
+	
+
+}
