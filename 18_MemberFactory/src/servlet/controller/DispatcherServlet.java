a95bac871bc41bad42c5e6a1774fb69a075376d4
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-08-11 00:51:16 +0900

    18_MemberFactory 내용 추가

--- 18_MemberFactory/src/servlet/controller/DispatcherServlet.java
+++ 18_MemberFactory/src/servlet/controller/DispatcherServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("*.do")
+@WebServlet("*.do") // 폼 action의 값이 .do 로 되어 있는 것은 모두 여기로 보내진다. 
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,14 +25,19 @@ public class DispatcherServlet extends HttpServlet {
 		
 		String command = requestURILIST[requestURILIST.length-1];
 		System.out.println("Command::" + command);
+		// 1. 위 세가지 루틴으로 command를 추출시킨다. command = 폼의 action 값
 		
+		
+		//2. HandlerMapping 에 있는 createController 불러옴(command값과 일치하면 관련 Controller불러옴)
 		Controller controller = HandlerMapping.getInstance().createController(command);
 		try {
+			//  3. 해당 컨트롤러의 handle메소드 호출 -> new ModelAndView(path,[true]) 반환
 			ModelAndView mv = controller.handle(request, response);
-			if(mv!=null) {
-				if(mv.isRedirect()) {
+			
+			if(mv!=null) { //4. mv 안에  객체가 존재한다면 
+				if(mv.isRedirect()) { //4-1. isRedirect 가 true면 네비게이션 sendRedirect 사용
 				  response.sendRedirect(mv.getPath());	
-				}else {
+				}else {//4-2. false면 네비게이션 getRequestDispatcher 사용
 					request.getRequestDispatcher(mv.getPath()).forward(request, response);
 				}
 			}
--- 18_MemberFactory/src/servlet/controller/HandlerMapping.java
+++ 18_MemberFactory/src/servlet/controller/HandlerMapping.java
@@ -10,13 +10,17 @@ import servlet.controller.component.UpdateController;
 // ControllerFactory 랑 하는 역할 같음 이름만 바뀜
 public class HandlerMapping {
 
+	// 1. HandlerMapping 객체 생성 
 	private static HandlerMapping handler = new HandlerMapping();
 	
+	// 2. 빈 생성자 
 	private HandlerMapping() {}
+	
+	//3. handler 반환하는 getInstance() 메소드 
 	public static HandlerMapping getInstance() {
 		return handler;
 	}
-	
+	//4. controller 만들어 내는 메소드 
 	public Controller createController(String command) {
 		Controller controller = null;
 		
