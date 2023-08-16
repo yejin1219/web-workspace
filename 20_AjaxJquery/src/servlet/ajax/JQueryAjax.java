40516f6d9b1610b12286476d7b8ea11fa13c99ba
johnmayer <133620338+yejin1219@users.noreply.github.com>
2023-08-16 17:44:48 +0900

    20_AjaxJquery

---
+++ 20_AjaxJquery/src/servlet/ajax/JQueryAjax.java
@@ -0,0 +1,39 @@
+package servlet.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/JQueryAjax")
+public class JQueryAjax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String pass = request.getParameter("pass");
+		
+		// error 발생할 수 있도록 응답 일부러 지연
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {}
+		
+		
+		// 실제 응답이 이뤄지는 부분
+		PrintWriter out = response.getWriter();
+		out.print(id + ", " + pass);
+		out.close();
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
